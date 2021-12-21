efe87d83d3fd9879ebfed3b792e6dd997e1ed817
berg-1 <884441972@qq.com>
2021-12-21 14:32:33 +0800

    修改index的footer样式；更改明暗主题；跟随系统的主题变化；删除一些不必要的壁纸

---
+++ src/main/resources/static/js/init-redirect.js
@@ -0,0 +1,8 @@
+function home() {
+    window.location.href = "index";
+}
+
+function redirectBili() {
+    // similar behavior as an HTTP redirect
+    window.location.replace("https://space.bilibili.com/12517262");
+}
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -14,13 +14,20 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+// 更改主题(黑夜-白天)  根据系统颜色做出自动调整
 const themeSwitch = document.querySelector('input');
 themeSwitch.addEventListener('change', () => {
-    document.body.classList.toggle('dark-theme');  // change web mode
+    const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)');
+    if (mediaQueryListDark.matches) {
+        document.body.classList.toggle('light-theme');
+    } else {
+        document.body.classList.toggle('dark-theme');
+    }
     togglePrismTheme()  // switch prism theme
 });
 
 jQuery(function () {
     // Handler for .ready() called.
 });
+
 
