293da3e0f2b30b53196a08955aa4a542baed9359
berg-1 <884441972@qq.com>
2022-01-08 19:00:14 +0800

    添加一些注释

--- src/main/resources/static/js/init-redirect.js
+++ src/main/resources/static/js/init-redirect.js
@@ -1,7 +1,13 @@
+/**
+ * Back to home page
+ */
 function home() {
     window.location.href = "index";
 }
 
+/**
+ * Redirect to bilibili home page
+ */
 function redirectBili() {
     // similar behavior as an HTTP redirect
     window.location.replace("https://space.bilibili.com/12517262");
