2bd8db4905c0a34bcb9d21bfd8dab0e21480106e
berg-1 <884441972@qq.com>
2022-01-20 13:05:32 +0800

    delete test

--- src/test/java/me/berg/home/HomeApplicationTests.java
+++ src/test/java/me/berg/home/HomeApplicationTests.java
@@ -4,10 +4,8 @@ import me.berg.home.service.RedisService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("dev")
 class HomeApplicationTests {
 
     @Autowired
