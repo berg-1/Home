0a017688c8874b864f4cbfe7a106b08f78da97de
berg-1 <884441972@qq.com>
2022-01-20 13:12:07 +0800

    配置文件修改

--- src/test/java/me/berg/home/HomeApplicationTests.java
+++ src/test/java/me/berg/home/HomeApplicationTests.java
@@ -1,5 +1,7 @@
 package me.berg.home;
 
+import me.berg.home.model.MyFile;
+import me.berg.home.service.MyFileService;
 import me.berg.home.service.RedisService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,19 @@ class HomeApplicationTests {
     @Autowired
     RedisService redisService;
 
+    @Autowired
+    MyFileService myFileService;
+
     @Test
-    void contextLoads() {
+    void redisTest() {
         String get = redisService.get("RedisTest");
         System.out.println(get);
+    }
+
+    @Test
+    void sqlTest(){
+        MyFile myFile = myFileService.getById("06d2a898-ab2c-44fd-9009-5cc0ea318c68");
+        assert myFile.getFilename().equals( "a.py");
     }
 
 }
